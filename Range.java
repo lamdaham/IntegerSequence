27e8d07c93f3e2e347899445da67b075c15e2a90
lamdaham <ilam21@stuy.edu>
2020-12-03 17:15:09 -0500

    finished range.java

--- Range.java
+++ Range.java
@@ -4,13 +4,29 @@ public class Range implements IntegerSequence{
 
   //@param start : the starting value (inclusive) which must be <= end.
   //@param end : the ending value which is also inclusive.
-  public Range(int start,  int end){  }
+  public Range(int start,  int end){
+  	start = start;
+  	end = end;
+  	current = start;
+  }
 
-  public void reset(){  }
-  public int length(){  }
-  public boolean hasNext(){  }
+  public void reset(){
+  	current = start;
+  }
+  public int length(){
+  	return (Math.abs(end-start));
+  }
+  public boolean hasNext(){
+  	return (!(current==end));
+  }
 
   //@throws NoSuchElementException when hasNext() is false.
-  public int next(){    }
+  public int next(){
+  	if (hasNext()) {
+  		return current+1;
+  	} else {
+  		throw new NoSuchElementException();
+  	}
+  }
   
 }
